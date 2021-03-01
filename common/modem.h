a45e4d2b8f8ab113f28ad574dea74979e9249457
Samuel Deutsch <sam.e.deutsch@gmail.com>
2021-03-01 02:07:56 -0500

    Finished build system

--- common/modem.h
+++
@@ -1,9 +0,0 @@
-#include <sys/ioctl.h>
-
-// Return the status of a modem's data carrier detect line
-inline int getDCD( int fd );
-// Return the status of a modem's data terminal ready line
-inline int getDTR( int fd );
-// Set the status of a modem's data terminal ready line
-inline void setDTR( int fd, int enable );
-
